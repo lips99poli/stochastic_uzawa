d5d754bbcddfaf2ef99ac6cd1260b6583525ff60
lips99poli <filippo.lipari@mail.polimi.it>
2025-09-03 13:15:51 +0000

    include omp

--- include/CommonTypes.hpp
+++ include/CommonTypes.hpp
@@ -5,10 +5,14 @@
 #include <Eigen/Dense>
 #include <cmath>
 
+#include <omp.h>
+
 // Common type aliases used throughout the project
 using Matrix = Eigen::MatrixXd;
 using Vector = Eigen::VectorXd;
 using Ref = Eigen::Ref<Matrix>;
 using MatVec = std::vector<Matrix>;
+
+using par_for_type = std::ptrdiff_t; // Type for parallel loop indices, compatible with OpenMP
 
 #endif // COMMON_TYPES_HPP
